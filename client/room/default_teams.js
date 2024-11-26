456d053f1aef6a2e512a00e98778259e6bf96d42
xwixyy <0xh0ld3r@gmail.com>
2024-11-26 21:32:05 +0300

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -1,8 +1,8 @@
 import { Color } from 'pixel_combats/basic';
 import { Teams } from 'pixel_combats/room';
 
-export const RED_TEAM_NAME = "Red";
-export const BLUE_TEAM_NAME = "Blue";
+export const RED_TEAM_NAME = "midx";
+export const BLUE_TEAM_NAME = "midx";
 export const RED_TEAM_DISPLAY_NAME = "Teams/Red";
 export const BLUE_TEAM_DISPLAY_NAME = "Teams/Blue";
 export const BLUE_TEAM_SPAWN_POINTS_GROUP = 1;
